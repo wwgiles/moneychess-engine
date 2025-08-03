69ee559ace18d2509ddbfb54644f72872923eb8a
wwgiles <wwg488@gmail.com>
2025-08-03 10:19:26 +0100

    Create CalculatePoints.py
    Calculates the number of points spent by the player

---
+++ HelperFunctions/CalculatePoints.py
@@ -0,0 +1,9 @@
+
+
+
+def calculate_points(pieces):
+    """Calculates total points spent on a list of pieces."""
+    total_points = 0
+    for piece_type in pieces:
+        total_points += PIECE_POINTS.get(piece_type, 0)
+    return total_points
