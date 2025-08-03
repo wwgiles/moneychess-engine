8e9372481db2382dc9a5970b3285a07acad80bd3
wwgiles <wwg488@gmail.com>
2025-08-03 10:00:08 +0100

    github desktop test

--- function_app.py
+++ function_app.py
@@ -3,7 +3,7 @@ import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-@app.route(route="http_trigger")
+@app.route(route="http_trigger_test")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
